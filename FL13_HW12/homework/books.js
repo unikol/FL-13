ef9648c435cccbd34c31af10a68dae6576eab1d4
Mykola Stepanov <ukrnikolas@gmail.com>
2020-05-17 23:26:11 +0300

    hw12_not_d

---
+++ FL13_HW12/homework/books.js
@@ -0,0 +1,63 @@
+// const data = [{bookName: 'a',
+// author: 'b',
+// img: 'c',
+// plot: 'd},{},{}]
+
+// localStorag.setItem('Books', JSON.stringify(books))
+
+
+// myStorage.setItem(id, JSON.stringify({
+//     title: i['title'];
+// }));
+
+
+let dynamicPart, newBook, data;
+
+
+let booksFromLS = () => {
+    return JSON.parse(localStorage.getItem('books'));
+    };
+
+dynamicPart.innerHTML = `
+        <form class="add-book">
+        <div class="title">
+          <label>
+            Назва книги: 
+            <input required type="text" placeholder="Назва книги">
+          </label>
+        </div>`
+
+
+        data.push(newBook);
+localStorage.setItem('books', JSON.stringify(data));
+
+
+const books = [
+    {
+        'id': '01',
+        'title': 'title 01',
+        'author': 'author 01',
+        'img': 'img 01',
+        'description': 'description 01'
+    },
+    {
+        'id': '02',
+        'title': 'title 02',
+        'author': 'author 02',
+        'img': 'img 02',
+        'description': 'description 02'},
+    {
+        'id': '03',
+        'title': 'title 03',
+        'author': 'author 03',
+        'img': 'img 03',
+        'description': 'description 03'}
+    ];
+    
+    const _new = {
+        'id': '03',
+        'title': 'title 03',
+        'author': 'author 03',
+        'img': 'img 03',
+        'description': 'description 03'
+    }
